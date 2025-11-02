be52a27811222b9a03032280c190005e79d989c0
Sanjeet Kumar <sanjeet.sks8271@gmail.com>
2025-11-03 00:42:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,6 +84,18 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
-addEntryButton.addEventListener("click", addEntry);
+function clearForm() {
+  const inputContainers = Array.from(document.querySelectorAll('.input-container'));
 
-// To be continued here...
+  for (const container of inputContainers) {
+    container.innerHTML = '';
+  }
+
+  budgetNumberInput.value = '';
+  output.innerText = '';
+  output.classList.add('hide');
+}
+
+addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener('click',clearForm);
